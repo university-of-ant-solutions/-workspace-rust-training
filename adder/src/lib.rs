4d0c5277c1ffe5d53c1a5be3c04dbc87e07cc636
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-10-21 18:46:40 +0700

    src: update adder func

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,7 +1,12 @@
+pub fn adder(a: u64, b: u64) -> u64 {
+    a + b
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(adder(2, 2), 4);
     }
 }
